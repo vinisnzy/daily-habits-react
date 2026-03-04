6451c0a3b72f227448d543569a60f33a1ee17504
Vinícius de Paula <manoseilacara123@gmail.com>
2026-03-03 21:39:36 -0300

    feat: add habits state, create function to toggle habits, and add new components to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
+import { useState } from 'react'
+import ListHabits from './components/ListHabits'
+import './styles/App.css'
+
 function App() {
+  const [habits, setHabits] = useState([
+    {
+      id: 1,
+      text: 'Tomar 3L de Água',
+      completed: false,
+    },
+    {
+      id: 2,
+      text: 'Ir à academia',
+      completed: true,
+    },
+  ])
+
+  function toggleHabit(id) {
+    const newHabits = habits.map((habit) =>
+      habit.id === id ? { ...habit, completed: !habit.completed } : habit,
+    )
+
+    setHabits(newHabits)
+  }
+
   return (
-    <div>
-      <h1>Hello, world!</h1>
+    <div className="container">
+      <h1 className="main-title">Daily Habits</h1>
+      <ListHabits habits={habits} toggleHabit={toggleHabit} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
